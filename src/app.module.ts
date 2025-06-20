bfbd606b2bea20b12e9808e087527b69655fbe51
Basantashrestha <basantashrestha@Basantas.local>
2025-06-20 16:46:45 +0545

    Creating controller with CLI

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TweetModule } from './tweet/tweet.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [UsersModule, TweetModule],
-  controllers: [AppController],
+  controllers: [AppController, UsersModule],
   providers: [AppService],
 })
 export class AppModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UserController } from './user.controller'; // Import the controller
+import { UsersController } from './users.controller';
+import { UsersController } from './users.controller';
 
-@Module({})
+@Module({
+    controllers: [UserController, UsersController], // Use the correct class name
+})
 export class UsersModule {}
