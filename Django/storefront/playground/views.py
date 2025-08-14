562ea46548c0074aeeb8bfa92d8159ff9c66823b
Basantashrestha <basantashrestha@Basantas.local>
2025-08-14 12:02:08 +0545

    Ignore Python cache files

--- Django/storefront/playground/views.py
+++ Django/storefront/playground/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def say_hello(request):
+    return HttpResponse('Hi , i am testing http request')
