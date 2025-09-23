1a52a51786485b59d522fc40a719db007a44ec8d
basan-ta <basanta.077bei008@ioepc.edu.no>
2025-09-23 22:14:26 +0545

    create tour class and database model

--- Django_practice_02/praticeproject/project2/models.py
+++ Django_practice_02/praticeproject/project2/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Tour(models.Model):
+    # we neec origin ,country , destination , price , duration , date 
+    origin_country = models.CharField(max_length = 60 )
+    destination_country = models.CharField(max_length = 60 )
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    duration = models.IntegerField()
+    date = models.DateField()
+
